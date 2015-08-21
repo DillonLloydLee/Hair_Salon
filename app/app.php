2a4657f586277b3c4a81f45991f1c8cca3c52b42
Dillon Lloyd Lee <crazycapn@gmail.com>
2015-08-21 10:49:53 -0700

    add stylist_edit.html.twig.

--- app/app.php
+++ app/app.php
@@ -17,16 +17,19 @@
         'twig.path' => __DIR__.'/../views'
     ));
 
+    // home route.
     $app->get("/", function() use ($app) {
         return $app['twig']->render('index.html.twig',
             array('stylists' => Stylist::getAll()));
     });
 
+    // add a client route.
     $app->post("/clients", function() use ($app) {
         $description = $_POST["description"];
         $stylist_id = $_POST["stylist_id"];
         $appointment = $_POST["appointment"];
-        $client = new Client($description, $id = null, $stylist_id, $appointment);
+        $client = new Client($description, $id = null,
+            $stylist_id, $appointment);
         $client->save();
         $stylist = Stylist::find($stylist_id);
         return $app["twig"]->render("stylist.html.twig",
@@ -34,6 +37,7 @@
             $stylist->getClients()));
     });
 
+    // particular stylist route.
     $app->get("/stylists/{id}", function($id) use ($app) {
         $stylist = Stylist::find($id);
         return $app['twig']->render('stylist.html.twig',
@@ -41,12 +45,14 @@
             $stylist->getClients()));
     });
 
+    // delete all clients route.
     $app->post("/deleted_clients", function() use ($app) {
         Client::deleteAll();
         return $app['twig']->render('index.html.twig',
             array('stylists' => Stylist::getAll()));
     });
 
+    // add a stylist route.
     $app->post("/stylists", function() use ($app) {
         $stylist = new Stylist($_POST["name"]);
         $stylist->save();
@@ -54,12 +60,19 @@
             array("stylists" => Stylist::getAll()));
     });
 
+    // delete all stylists route.
     $app->post("/deleted_stylists", function() use ($app) {
         Stylist::deleteAll();
         return $app['twig']->render('index.html.twig',
             array('stylists' => Stylist::getAll()));
     });
 
+    // change a stylist's name route.
+    $app->get("/stylists/{id}/edit", function($id) use ($app) {
+        $stylist = Stylist::find($id);
+        return $app["twig"]->render("stylist_edit.html.twig", array("stylist" => $stylist));
+    });
+
     return $app;
-    
+
 ?>
